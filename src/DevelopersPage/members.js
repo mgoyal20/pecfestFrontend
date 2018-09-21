6e27c2fc6d002cfc1eddf978ef2c3ab2e22f137c
mgoyal20 <mgoel20@gmail.com>
2018-09-21 23:29:19 +0530

    Updating team

--- src/DevelopersPage/members.js
+++ src/DevelopersPage/members.js
@@ -1,52 +1,54 @@
-import prince from './images/prince.jpg';
-import harman from './images/harman.jpg';
-import mitesh from './images/mitesh.jpg';
-import ankit from './images/image.jpg';
-import aknsha from './images/profile.png';
-import roy from './images/profile.png';
+import profile from './images/profile.png';
 
 export default [
 	{
 		id: 1,
-		name: 'Pushpinder Singh',
-		github: 'https://github.com/princedhaliwal',
-		linkedIn: 'https://goo.gl/5ovkci',
-		photo: prince,
-		work: 'Frontend & Backend',
-		color: 'rgba(124, 143, 105, 0.5)',
-		facebook: 'https://www.facebook.com/0x1000',
+		name: 'Mayank Goyal',
+        work: 'Website Coordinator',
+        photo: profile,
+        color: 'rgba(124, 143, 105, 0.5)',
+		github: 'https://github.com/mgoyal20',
 	},
-	{
-		id: 2,
-		photo: mitesh,
-		work: 'Frontend & Backend',
-		name: 'Mitesh Kakkar',
-		color: 'rgba(136, 109, 83, 0.5)',
-	},
-	{
-		id: 3,
-		photo: roy,
-		work: 'Frontend & Backend',
-		name: 'Shubham Roy'
-	},
-	{
-		id: 4,
-		photo: ankit,
-		work: 'Backend',
-		name: 'Ankit Bindal',
-		color: 'rgba(182, 141, 117, 0.8)'
-	},
-	{
-		id: 5,
-		photo: aknsha,
-		work: 'Frontend',
-		name: 'Aakankasha Sharma',
-	},
-	{
-		id: 6,
-		photo: harman,
-		work: 'Android App',
-		color: 'rgba(58, 81, 131, 0.5)',
-		name: 'Harmanpreet Singh'
-	}
+    {
+        id: 2,
+        name: 'Shyam Pandya',
+        work: 'Website Coordinator',
+        photo: profile,
+        color: 'rgba(182, 141, 117, 0.8)',
+        github: '',
+    },
+    {
+        id: 3,
+        name: 'Ritesh Goyal',
+        work: 'Frontend and App',
+        photo: profile,
+        color: 'rgba(124, 143, 105, 0.5)',
+        github: '',
+    },
+    {
+        id: 4,
+        name: 'Aakankasha Sharma',
+        work: 'Frontend and Backend',
+        photo: profile,
+        color: 'rgba(182, 141, 117, 0.8)',
+        github: '',
+        linkedIn: '',
+        facebook: '',
+    },
+    {
+        id: 5,
+        name: 'Krittika Chhabra',
+        work: 'Frontend',
+        photo: profile,
+        color: 'rgba(124, 143, 105, 0.5)',
+        github: '',
+    },
+    {
+        id: 6,
+        name: 'Vaibhav Wimpsta',
+        work: 'Frontend',
+        photo: profile,
+        color: 'rgba(182, 141, 117, 0.8)',
+        github: '',
+    }
 ]
