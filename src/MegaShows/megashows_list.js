f02e41363d9d9dba6a7116aceb5f8e9835b33842
mgoyal20 <mgoel20@gmail.com>
2018-10-25 02:08:20 +0530

    Updating Megashows

--- src/MegaShows/megashows_list.js
+++ src/MegaShows/megashows_list.js
@@ -1,37 +1,48 @@
-import farhanPicture from '../images/farhan.jpg';
+import ss from '../images/SS (1).jpg';
 import glit from '../images/glit.jpg';
 import bhangra from '../images/bhangra.jpg';
 import groovz from '../images/groovz.jpg';
+import tlt from '../images/TLT pfsdt2-min.jpg';
 
 export default [
     {
         id: 1,
-        title: 'Star Night Pecfest 2017',
-        titleLine: 'Farhan Live',
-        date: '',
-        location: '',
-        coverPhoto: farhanPicture,
+        title: 'Star Night',
+        titleLine: 'Sukhwinder Singh Live',
+        date: '28th October 2018',
+        location: 'Main Stage',
+        coverPhoto: ss,
+        isEvent: false,
+        shouldSplit: true,
+    },
+    {
+        id: 2,
+        title: 'Pro Night',
+        titleLine: 'The Local Train',
+        date: '27th October 2018',
+        location: 'Main Stage',
+        coverPhoto: tlt,
         isEvent: false,
         //poster: '/Images/shows/Farhan.jpg',
         shouldSplit: true,
     },
     {
-        id: 2,
-        title: 'Groovz',
+        id: 3,
+        title: 'Groovz Finals',
         titleLine: 'Bollywood Dance',
-        date: '',
-        location: '',
+        date: '26th October 2018',
+        location: 'Main Stage',
         coverPhoto: groovz,
         shouldSplit: false,
         isEvent: false,
         //eventId: '/events/119',
     },
     {
-        id: 3,
-        title: 'Gliterrati',
+        id: 4,
+        title: 'Gliterrati Finals',
         titleLine: 'Fashion Night',
-        date: '',
-        location: '',
+        date: '26th October 2018',
+        location: 'Main Stage',
         coverPhoto: glit,
         //poster: '/Images/shows/gl.jpg',
         isEvent: false,
@@ -39,11 +50,11 @@ export default [
         shouldSplit: false,
     },
     {
-        id: 4,
-        title: 'Bhangra Theque',
+        id: 5,
+        title: 'Bhangra Theque Finals',
         titleLine: 'Beat the Punjabi Beats',
-        date: '',
-        location: '',
+        date: '26th October 2018',
+        location: 'Main Stage',
         coverPhoto: bhangra,
         //poster: '/Images/shows/bhangraT.jpg',
         isEvent: false,
